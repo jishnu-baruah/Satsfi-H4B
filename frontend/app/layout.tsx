5bbddcefae6295c8146f72df352b9575643e5c6d
jishnu-baruah <jsbaruah1@gmail.com>
2025-06-21 13:30:11 +0530

    fixed production server

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <ClientRoot>
-            {children}
+        {children}
           </ClientRoot>
         </ThemeProvider>
       </body>
