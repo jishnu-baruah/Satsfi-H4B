768c5305b83b41158e4809b8153fe350c19ba39e
jishnu-baruah <jsbaruah1@gmail.com>
2025-06-21 14:31:20 +0530

    added coin gecko api

--- backend/services/priceCacheService.js
+++ backend/services/priceCacheService.js
@@ -1,12 +1,17 @@
 const axios = require('axios');
 
 const COIN_GECKO_IDS = ["bitcoin", "ethereum", "usd-coin", "tether", "coredaoorg"].join(',');
-const COIN_GECKO_URL = `https://api.coingecko.com/api/v3/simple/price?ids=${COIN_GECKO_IDS}&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true`;
+const API_KEY = process.env.COINGECKO_API_KEY;
+
+// Use the Pro API endpoint if an API key is provided, otherwise fallback to the public API.
+const COIN_GECKO_URL = API_KEY 
+    ? `https://pro-api.coingecko.com/api/v3/simple/price?ids=${COIN_GECKO_IDS}&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true&x_cg_pro_api_key=${API_KEY}`
+    : `https://api.coingecko.com/api/v3/simple/price?ids=${COIN_GECKO_IDS}&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true`;
 
 class PriceCacheService {
     constructor() {
         this.priceCache = {};
-        this.fetchInterval = 60000; // Fetch every 60 seconds
+        this.fetchInterval = 300000; // Fetch every 5 minutes
     }
 
     async fetchAndCachePrices() {
